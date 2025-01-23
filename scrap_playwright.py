91102b690a65473c4c84f75dc8f70c3ef022db90
collmas <llcollmas@uoc.edu>
2025-01-24 00:38:01 +0100

    Updates scrap & sheet functions

--- scrap_playwright.py
+++ scrap_playwright.py
@@ -2,6 +2,8 @@ import re
 
 from playwright.sync_api import sync_playwright
 
+import sheets_funcs
+
 URL_BASE = "https://treball.barcelonactiva.cat/porta22/cat/assetsocupacio/ofertesfeina/sectors.jsp?sector=SECTOR&idioma=cat"
 SECTORS = [
     "40271",
@@ -55,9 +57,35 @@ def obtenir_dades_sectors(url):
 def check_key_words(job): 
     return re.search(KEYWORDS_PATTERNS, job, re.I)
 
-# Obté les ofertes i mostra-les
-for sector in SECTORS:
-    ofertes = obtenir_dades_sectors(URL_BASE.replace("SECTOR", sector))
-    print(f"Scraping {URL_BASE.replace('SECTOR', sector)}...")
-    for oferta in ofertes:
-        print(oferta)
+def get_new_jobs(jobs):
+    df = sheets_funcs.get_sheet_as_df(sheets_funcs._worksheet)
+    new_jobs = []
+    for job in jobs:
+        if sheets_funcs.check_if_job_exists(df, job["link"]):
+            new_jobs.append(job)
+    return new_jobs
+
+def update_new_jobs(jobs):
+    df = sheets_funcs.get_sheet_as_df(sheets_funcs._worksheet)
+    for job in jobs:
+        length = sheets_funcs.get_column_length(df)
+        df.loc[length] = job
+    sheets_funcs.update_sheets(sheets_funcs._worksheet, df)
+
+def main():
+    feines = []
+    feines_noves = []
+    for sector in SECTORS:
+        print(f"Scraping {URL_BASE.replace('SECTOR', sector)}...")
+        jobs = obtenir_dades_sectors(URL_BASE.replace("SECTOR", sector))
+        new_jobs = get_new_jobs(jobs)
+        print(f"Hem trobat {len(jobs)} feines, de les quals {len(new_jobs)} són noves.")
+        update_new_jobs(new_jobs)
+        feines = feines + jobs
+        feines_noves = feines_noves + new_jobs
+    print(f"feines noves: {feines_noves}")
+    print(f"fines antigues: {[feina for feina in feines if feina not in feines_noves]}")
+
+
+if __name__ == "__main__":
+    main()
